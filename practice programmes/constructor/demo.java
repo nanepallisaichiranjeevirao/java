a73c9bdf092a8f6e4031ac31d17814aa9880aad6
nanepallisaichiranjeevirao <307544680+nanepallisaichiranjeevirao@users.noreply.github.com>
2026-08-09 23:39:47 +0530

    Update demo.java

--- practice programmes/constructor/demo.java
+++ practice programmes/constructor/demo.java
@@ -1,1 +1,32 @@
+package javacore;
 
+public class ConDemo {
+// class or instance variables
+	int rollNo;
+    String name;
+    float fee;
+	
+	// constructor 
+	public ConDemo() { 
+	 this.rollNo = 240;
+	 this.name= "chiru";
+	 this.due= 60999F;
+	}
+	void sample(){
+		System.out.println("ROLL NO is:" + this.rollNo);
+		System.out.println("NAME is:" + this.name);
+		System.out.println("DUE is:" + this.due);
+	}
+	void example() {
+		System.out.println("some other method named example");
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("This is main method..!");
+		ConDemo cd = new ConDemo();
+		cd.sample();
+		cd.example();
+	}
+		
+	}
