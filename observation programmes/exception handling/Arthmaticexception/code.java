3025a4d33413609ea7d706a6e1f2cdfb8b7558eb
nanepallisaichiranjeevirao <307544680+nanepallisaichiranjeevirao@users.noreply.github.com>
2026-09-07 00:24:38 +0530

    Update code.java

--- observation programmes/exception handling/Arthmaticexception/code.java
+++ observation programmes/exception handling/Arthmaticexception/code.java
@@ -1,1 +1,12 @@
-
+public class ArithmeticExceptionExample {
+   public static void main(String[] args) {
+      int a = 10;
+      int b = 0;
+      try {
+         int c = a/b;
+         System.out.println(c);
+      } catch(ArithmeticException e) {
+         System.out.println("Can't divide a number by zero");
+      }
+   }
+}
