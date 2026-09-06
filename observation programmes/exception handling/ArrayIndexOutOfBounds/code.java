52dc0c4e4b48534e33ff8122308cb57a28fc498c
nanepallisaichiranjeevirao <307544680+nanepallisaichiranjeevirao@users.noreply.github.com>
2026-09-07 00:18:27 +0530

    Update code.java

--- observation programmes/exception handling/ArrayIndexOutOfBounds/code.java
+++ observation programmes/exception handling/ArrayIndexOutOfBounds/code.java
@@ -1,1 +1,10 @@
-
+class Example {
+    public static void main(String[] args) {
+        try {
+            int[] a = {10, 20, 30};
+            System.out.println(a[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index is invalid");
+        }
+    }
+}
