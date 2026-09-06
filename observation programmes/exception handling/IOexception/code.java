19b954ef1986f87cca78dd414a318fc90a41bfea
nanepallisaichiranjeevirao <307544680+nanepallisaichiranjeevirao@users.noreply.github.com>
2026-09-07 00:31:03 +0530

    Update code.java

--- observation programmes/exception handling/IOexception/code.java
+++ observation programmes/exception handling/IOexception/code.java
@@ -1,1 +1,28 @@
 
+import java.io.*;
+public class Main {
+    
+    public static void main(String[] args) throws IOException
+    {
+
+      
+        try {
+            
+            FileReader file = new FileReader("file.txt");
+
+            System.out.println(file.read());
+        }
+      
+
+        catch (Exception exp) {
+
+            System.out.println("Error occured: " + exp.getMessage());
+        }
+      
+        
+        finally {
+            
+            System.out.println( "Finally block will always executed, irrespective of occurence of issue");
+        }
+    }
+}
