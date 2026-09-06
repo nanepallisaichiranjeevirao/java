c2aceab2237e2fe8cd1464735b97b68d9278a435
nanepallisaichiranjeevirao <307544680+nanepallisaichiranjeevirao@users.noreply.github.com>
2026-09-07 00:25:21 +0530

    Update Code.java

--- observation programmes/exception handling/InputMismatch/Code.java
+++ observation programmes/exception handling/InputMismatch/Code.java
@@ -1,1 +1,14 @@
+import java.util.*;
 
+class Example8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter an integer: ");
+            int n = sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter a valid integer");
+        }
+    }
+}
