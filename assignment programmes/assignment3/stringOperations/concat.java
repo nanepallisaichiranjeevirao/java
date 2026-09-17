48b4e46f41524710d856bbae5a34f90c217fb9af
nanepallisaichiranjeevirao <307544680+nanepallisaichiranjeevirao@users.noreply.github.com>
2026-09-17 22:19:51 +0530

    Update concat.java

--- assignment programmes/assignment3/stringOperations/concat.java
+++ assignment programmes/assignment3/stringOperations/concat.java
@@ -1,1 +1,8 @@
 
+class Concat{
+    public static void main(String[] args) {
+        String first = "Hello";
+        String second = " World";
+        System.out.println(first + second);
+    }
+}
